d449853d7040fb1dd7a5b9c199759a9ad9e592cc
matiglesias <matidxg@gmail.com>
2020-08-21 11:53:58 -0300

    Added state and request to both Post and Comments

--- src/post/Comments.js
+++ src/post/Comments.js
@@ -2,6 +2,18 @@ import React, { useState, useEffect } from 'react';
 import Backend from '../services/backend';
 
 const Comments = ({ postID }) => {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    const backend = new Backend();
+    backend
+      .getComments(postID)
+      .then((result) => setComments(result.data || []))
+      .catch((err) => console.log(backend.isCancelled(err)));
+
+    return () => backend.dispose();
+  }, [postID]);
+
   return (
   );
 };
--- src/post/Post.js
+++ src/post/Post.js
@@ -2,6 +2,17 @@ import React, { useState, useEffect } from 'react';
 import Backend from '../services/backend';
 
 const Post = ({ postID }) => {
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {
+    const backend = new Backend();
+    backend
+      .getPost(postID)
+      .then((result) => setPost(result.data || []))
+      .catch((err) => console.log(backend.isCancelled(err)));
+
+    return () => backend.dispose();
+  }, [postID]);
   return (
   );
 };
