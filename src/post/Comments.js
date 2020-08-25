9b64493ff0fb1839c868a7870973fc52f24ea921
matiglesias <matidxg@gmail.com>
2020-08-25 17:56:21 -0300

    Added styling to comments

--- src/post/Comments.js
+++ src/post/Comments.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Backend from '../services/backend';
+import moment from 'moment';
 
 const Comments = ({ postID }) => {
   const [comments, setComments] = useState([]);
@@ -14,8 +15,34 @@ const Comments = ({ postID }) => {
     return () => backend.dispose();
   }, [postID]);
 
+  const showComment = (c, index) => {
+    return (
+      <div key={index} className="comment">
+        <a href="#goToUser" className="avatar">
+          <img src="avatars/avatar1.jpg" alt="Avatar" />
+        </a>
+        <div className="content">
+          <a href="#goToUser" className="author">Autor</a>
+          <div className="metadata">
+            <span className="date">{moment(c.createdAt).fromNow()}</span>
+          </div>
+          <div className="text">
+            {c.content}
+          </div>
+          <div className="actions">
+            <a href="#showReplyForm" className="reply">Reply</a>
+            <a href="#showResponses">Show responses</a>
+          </div>
+        </div>
+      </div >
+    )
+  }
+
   return (
-    <div>{comments.map((c, index) => (<div key={index}> {c.content} </div>))}</div>
+    <div className="ui comments text container">
+      <h3 className="ui dividing header">Comments</h3>
+      {comments.map(showComment)}
+    </div>
   );
 };
 
