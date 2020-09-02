6dec082cc0a580b4059ed13df9b4b39af8a06e63
matiglesias <matidxg@gmail.com>
2020-09-02 20:36:35 -0300

    Added Navbar component

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className="ui fixed inverted menu">
+      <div className="ui container">
+        <a href="/" className="header item" style={{ "fontSize": "1.32rem" }}>
+          Gauchadas
+          </a>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
