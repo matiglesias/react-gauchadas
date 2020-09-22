6d4cafa1968029887757a8272ff3c1a492ad7365
matiglesias <matidxg@gmail.com>
2020-09-22 12:07:35 -0300

    Some temporal things to make branch work

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import PostView from './components/post/PostView'
+import Navbar from './components/navbar/Navbar'
+
+const postID = "5f5b9fd030955bc7142f9715" // Should get it from route
 
 function App() {
   return (
     <>
+      <Navbar />
+      <PostView postID={postID} />
+      <br></br>
     </>
   );
 }
